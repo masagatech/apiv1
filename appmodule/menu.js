fa472dabda30377601005072022240432118ecd7
vivekpandey12 <vivek.pandey5188@gmail.com>
2016-12-13 19:57:15 +0530

    add new method in menu service with route

--- appmodule/menu.js
+++ appmodule/menu.js
@@ -4,8 +4,16 @@ var globals = require("../globals.js");
 
 var menu = module.exports = {};
 
-menu.getmenu = function getmenu(req, res, done) {
-    db.callProcedure("select " + globals.schema("funget_menuhead") + "($1,$2::json);", ['head', req.body], function(data) {
+menu.getMenuHead = function getMenuHead(req, res, done) {
+    db.callProcedure("select " + globals.schema("funget_menuhead") + "($1,$2,$3,$4::json);", ['head', 'head1', 'head2', req.body], function(data) {
+        rs.resp(res, 200, data.rows);
+    }, function(err) {
+        rs.resp(res, 401, "error : " + err);
+    }, 3)
+}
+
+menu.getMenu = function getMenu(req, res, done) {
+    db.callProcedure("select " + globals.schema("funget_menuhead") + "($1,'a','b',$2::json);", ['head', req.body], function(data) {
         rs.resp(res, 200, data.rows);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
