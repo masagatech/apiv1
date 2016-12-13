3b88b1da4ac23bc390cf8bffb94b31bca1e70e85
vivekpandey12 <vivek.pandey5188@gmail.com>
2016-12-13 19:57:55 +0530

    rename procedure funget_employee

--- appmodule/employee.js
+++ appmodule/employee.js
@@ -5,7 +5,7 @@ var globals = require("../globals.js");
 var employee = module.exports = {};
 
 employee.getEmployee = function getEmployee(req, res, done) {
-    db.callProcedure("select " + globals.schema("employee_get") + "($1,$2::json);", ['emp', req.body], function(data) {
+    db.callProcedure("select " + globals.schema("funget_employee") + "($1,$2::json);", ['emp', req.body], function(data) {
         rs.resp(res, 200, data.rows);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
