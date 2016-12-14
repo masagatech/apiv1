31165dad4f8d5c1c41594ef6414a3335e5df7a03
vivekpandey12 <vivek.pandey5188@gmail.com>
2016-12-14 20:13:09 +0530

    updated fy form

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 var items = require('../appmodule/items.js');
 var headmenu = require('../appmodule/menu.js');
 var common = require('../appmodule/common.js');
+var fy = require('../appmodule/fy.js');
 var user = require('../appmodule/user.js');
 var emp = require('../appmodule/employee.js');
 var acgroup = require('../appmodule/acgroup.js');
@@ -19,12 +20,19 @@ var appRouter = function(app) {
 
     //#################### Login / ##########################
     app.post("/getLogin", user.getLogin);
+    app.post("/getUsers", user.getUsers);
+    app.post("/saveUsers", user.saveUsers);
     //#############################################################################################
 
     //#################### Common / ##########################
     app.post("/getAutoData", common.getAutoData);
     app.post("/getMOM", common.getMOM);
     app.post("/saveMOM", common.saveMOM);
+    //#############################################################################################
+
+    //#################### FY / ##########################
+    app.post("/getfy", fy.getfy);
+    app.post("/savefy", fy.savefy);
     //#############################################################################################
 
     //#################### Employee / ##########################
