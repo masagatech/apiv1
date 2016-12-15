29e16e5e391d3eda5f2d46f9ba7d4d4f2267181b
vivekpandey12 <vivek.pandey5188@gmail.com>
2016-12-15 19:45:27 +0530

    update service for company

---
+++ appmodule/company.js
@@ -0,0 +1,38 @@
+var db = require("../db/dbservice.js");
+var rs = require("./util/resp.js");
+var globals = require("../globals.js");
+
+var company = module.exports = {};
+
+company.getCompany = function getCompany(req, res, done) {
+    
+    var params = [];
+    var paramstr = "";
+    var countr  = 1;
+    switch (req.body.flag) {
+        case "actwithfy":
+            params = ['company','company1', req.body];
+            paramstr = "($1,$2,$3::json);";
+            countr = 2;
+            break;
+        default:
+            params = ['company', req.body];
+            paramstr = "($1,'a',$2::json);";
+            countr = 1;
+            break;
+    }
+
+    db.callProcedure("select " + globals.schema("funget_company") + paramstr,params, function(data) {
+        rs.resp(res, 200, data.rows);
+    }, function(err) {
+        rs.resp(res, 401, "error : " + err);
+    }, countr)
+}
+
+company.saveCompany = function saveCompany(req, res, done) {
+    db.callFunction("select " + globals.schema("funsave_company") + "($1::json);", [req.body], function(data) {
+        rs.resp(res, 200, data.rows);
+    }, function(err) {
+        rs.resp(res, 401, "error : " + err);
+    })
+}
