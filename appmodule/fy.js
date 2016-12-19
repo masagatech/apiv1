6fa7a7df4d75ab9dd6746f39e6e139e1375da7b9
vivekpandey12 <vivek.pandey5188@gmail.com>
2016-12-19 20:52:49 +0530

    fy service changes

--- appmodule/fy.js
+++ appmodule/fy.js
@@ -19,3 +19,11 @@ fy.savefy = function savefy(req, res, done) {
         rs.resp(res, 401, "error : " + err);
     })
 }
+
+fy.saveCompanyFYMap = function savefy(req, res, done) {
+    db.callFunction("select " + globals.schema("funsave_companyfymap") + "($1::json);", [req.body], function(data) {
+        rs.resp(res, 200, data.rows);
+    }, function(err) {
+        rs.resp(res, 401, "error : " + err);
+    })
+}
