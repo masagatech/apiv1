c6b50a5f3f24c54429e346daee8c72830dcb2eb0
pratikn <pratikway.90@gmail.com>
2016-12-19 20:55:54 +0530

    user log changes

--- appmodule/user.js
+++ appmodule/user.js
@@ -5,12 +5,29 @@ var globals = require("../globals.js");
 var user = module.exports = {};
 
 user.getLogin = function getLogin(req, res, done) {
-    db.callProcedure("select " + globals.schema("funget_login") + "($1,$2::json);", ['login', req.body], function(data) {
+    var _data = getUserData(req);
+
+    db.callProcedure("select " + globals.schema("funget_login") + "($1,$2::json);", ['login', _data], function(data) {
         rs.resp(res, 200, data.rows);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
     }, 1)
 }
+
+function getUserData(req) {
+    var data = req.body;
+
+    var ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+    var other = data.otherdetails;
+    other.IP = ip;
+
+    data.other = other;
+    return data;
+}
+
 
 user.getUsers = function getUsers(req, res, done) {
     db.callProcedure("select " + globals.schema("funget_user") + "($1,$2::json);", ['users', req.body], function(data) {
