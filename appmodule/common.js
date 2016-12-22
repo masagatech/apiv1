79f75031c6e56377fe1ca7e6d5a09448ba06c013
vivekpandey12 <vivek.pandey5188@gmail.com>
2016-12-22 20:03:19 +0530

    make new service "checkValidate"

--- appmodule/common.js
+++ appmodule/common.js
@@ -12,6 +12,14 @@ common.getAutoData = function getAutoData(req, res, done) {
     }, 1)
 }
 
+common.checkValidate = function checkValidate(req, res, done) {
+    db.callProcedure("select " + globals.schema("funcheck_validate") + "($1,$2::json);", ['auto', req.body], function(data) {
+        rs.resp(res, 200, data.rows);
+    }, function(err) {
+        rs.resp(res, 401, "error : " + err);
+    }, 1)
+}
+
 common.getMOM = function getMOM(req, res, done) {
     db.callProcedure("select " + globals.schema("funget_mom") + "($1,$2::json);", ['mom', req.body], function(data) {
         rs.resp(res, 200, data.rows);
