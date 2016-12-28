92f53ab322c73c5b1d7daff72a91b8c3f8fa3ff9
vivekpandey12 <vivek.pandey5188@gmail.com>
2016-12-28 17:33:08 +0530

    changes in file upload service

--- appmodule/fileupload.js
+++ appmodule/fileupload.js
@@ -9,31 +9,47 @@ var fileupload = module.exports = {};
 
 fileupload.uploadFile = function uploadFile(req, res, done) {
 
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    
+    var guid = s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+
     var form = new formidable.IncomingForm();
+
     //Formidable uploads to operating systems tmp dir by default
+
     form.uploadDir = "./uploads"; //set upload directory
     form.keepExtensions = true; //keep file extension
-
+  
     form.parse(req, function(err, fields, files) {
         res.writeHead(200, { 'content-type': 'text/plain' });
-        res.write('received upload:\n\n');
+        res.write(JSON.stringify(files.fileUploaded));
         console.log("form.bytesReceived");
+
         //TESTING
+
         console.log("file size: " + JSON.stringify(files.fileUploaded.size));
         console.log("file path: " + JSON.stringify(files.fileUploaded.path));
         console.log("file name: " + JSON.stringify(files.fileUploaded.name));
         console.log("file type: " + JSON.stringify(files.fileUploaded.type));
         console.log("astModifiedDate: " + JSON.stringify(files.fileUploaded.lastModifiedDate));
+        console.log("guid: " + guid);
 
         //Formidable changes the name of the uploaded file
         //Rename the file to its original name
-        fs.rename(files.fileUploaded.path, './uploads/' + files.fileUploaded.name, function(err) {
-            if (err)
-                throw err;
-            console.log('renamed complete');
-        });
+
+        //var ext = files.fileUploaded.name.substr(files.fileUploaded.name.lastIndexOf('.') + 1);
+
+        // fs.rename('./uploads/' + files.fileUploaded.name, './uploads/' + guid + "." + ext, function(err) {
+        //     if (err)
+        //         throw err;
+        //     console.log('renamed complete');
+        // });
+        
         res.end();
     });
+
     // if (path.extname(req.files.file.name).toLowerCase() === '.png') {
     //     fs.rename(tempPath, targetPath, function(err) {
     //         if (err) throw err;
@@ -45,5 +61,4 @@ fileupload.uploadFile = function uploadFile(req, res, done) {
     //         console.error("Only .png files are allowed!");
     //     });
     // }
-
 }
