1225fb07a5c7742b03d6069b33cbaf787dff362d
vivekpandey12 <vivek.pandey5188@gmail.com>
2017-01-03 20:12:56 +0530

    file upload changes

--- appmodule/fileupload.js
+++ appmodule/fileupload.js
@@ -16,6 +16,7 @@ fileupload.uploadFile = function uploadFile(req, res, done) {
     var guid = s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 
     var form = new formidable.IncomingForm();
+    var i = 0;
 
     //Formidable uploads to operating systems tmp dir by default
 
@@ -25,27 +26,33 @@ fileupload.uploadFile = function uploadFile(req, res, done) {
     form.parse(req, function(err, fields, files) {
         res.writeHead(200, { 'content-type': 'text/plain' });
         res.write(JSON.stringify(files.fileUploaded));
+
+        i = i + 1;
+
         console.log("form.bytesReceived");
+
+        var name = JSON.stringify(files.fileUploaded.name);
+        var filepath = JSON.stringify(files.fileUploaded.path);
+        var size = JSON.stringify(files.fileUploaded.size);
+        var type = JSON.stringify(files.fileUploaded.type);
 
         //TESTING
 
-        console.log("file size: " + JSON.stringify(files.fileUploaded.size));
-        console.log("file path: " + JSON.stringify(files.fileUploaded.path));
-        console.log("file name: " + JSON.stringify(files.fileUploaded.name));
-        console.log("file type: " + JSON.stringify(files.fileUploaded.type));
+        console.log("file name: " + name);
+        console.log("file path: " + filepath);
+        console.log("file size: " + size);
+        console.log("file type: " + type);
         console.log("astModifiedDate: " + JSON.stringify(files.fileUploaded.lastModifiedDate));
         console.log("guid: " + guid);
 
         //Formidable changes the name of the uploaded file
         //Rename the file to its original name
 
-        //var ext = files.fileUploaded.name.substr(files.fileUploaded.name.lastIndexOf('.') + 1);
-
-        // fs.rename('./uploads/' + files.fileUploaded.name, './uploads/' + guid + "." + ext, function(err) {
-        //     if (err)
-        //         throw err;
-        //     console.log('renamed complete');
-        // });
+        fs.rename(files.fileUploaded.path, './uploads/' + files.fileUploaded.name, function(err) {
+            if (err)
+                throw err;
+            console.log('renamed complete');
+        });
         
         res.end();
     });
