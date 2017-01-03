e8b34005e29cfd30e4a90b9433c04c536acdfae8
zaidansari <azaid51@gmail.com>
2017-01-03 06:41:28 -0800

    customer master module

--- appmodule/customer.js
+++ appmodule/customer.js
@@ -5,11 +5,11 @@ var globals = require("../globals.js");
 var Customermaster = module.exports = {};
 
 Customermaster.getcustomerdrop = function getcustomerdrop(req, res, done) {
-    db.callProcedure("select " + globals.schema("funget_customerdrop") + "($1,$2,$3::json);", ['c1','c2', req.body], function(data) {
+    db.callProcedure("select " + globals.schema("funget_customerdrop") + "($1,$2,$3,$4::json);", ['c1','c2','c3', req.body], function(data) {
         rs.resp(res, 200, data.rows);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
-    }, 2)
+    }, 3)
 }
 
 Customermaster.getcustomer = function getcustomer(req, res, done) {
