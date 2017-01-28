775eee697cd29c0700dc7f415266d138bf5b501f
vivekpandey12 <vivek.pandey5188@gmail.com>
2017-01-28 20:16:47 +0530

    make validation service

--- appmodule/validation.js
+++ appmodule/validation.js
@@ -4,8 +4,16 @@ var globals = require("../globals.js");
 
 var valid = module.exports = {};
 
-valid.checkdatevalidate = function checkdatevalidate(req, res, done) {
-    db.callProcedure("select " + globals.schema("funcheck_datevalidate") + "($1,$2::json);", ['valid', req.body], function(data) {
+valid.checkFormValid = function checkFormValid(req, res, done) {
+    db.callProcedure("select " + globals.schema("funcheck_formvalid") + "($1,$2::json);", ['valid', req.body], function(data) {
+        rs.resp(res, 200, data.rows);
+    }, function(err) {
+        rs.resp(res, 401, "error : " + err);
+    }, 1)
+}
+
+valid.checkDateValid = function checkDateValid(req, res, done) {
+    db.callProcedure("select " + globals.schema("funcheck_datevalid") + "($1,$2::json);", ['valid', req.body], function(data) {
         rs.resp(res, 200, data.rows);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
