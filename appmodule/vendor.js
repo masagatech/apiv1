f4e394c9f5afa8a4c3ba77296cef5f3fb742f999
zaidansari <azaid51@gmail.com>
2017-01-31 22:39:10 -0800

    change  vendor master method

--- appmodule/vendor.js
+++ appmodule/vendor.js
@@ -5,11 +5,11 @@ var globals = require("../globals.js");
 var Vendormaster = module.exports = {};
 
 Vendormaster.getvendordrop = function getvendordrop(req, res, done) {
-    db.callProcedure("select " + globals.schema("funget_customerdrop") + "($1,$2,$3,$4::json);", ['c1','c2','c3', req.body], function(data) {
+    db.callProcedure("select " + globals.schema("funget_customerdrop") + "($1,$2,$3,$4,$5::json);", ['c1','c2','c3','c4', req.body], function(data) {
         rs.resp(res, 200, data.rows);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
-    }, 3)
+    }, 4)
 }
 
 Vendormaster.getvendor = function getvendor(req, res, done) {
