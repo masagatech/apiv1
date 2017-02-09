5f4a700bec3d929adae89854e5fcf02194b0d627
zaidansari <azaid51@gmail.com>
2017-02-09 06:44:07 -0800

    add new method

--- appmodule/wareopeningstock.js
+++ appmodule/wareopeningstock.js
@@ -6,11 +6,11 @@ var openstock = module.exports = {};
 
 
 openstock.getopeningstock = function getopeningstock(req, res, done) {
-    db.callProcedure("select " + globals.schema("funget_openingstock") + "($1,$2::json);", ['ops', req.body], function(data) {
+    db.callProcedure("select " + globals.schema("funget_openingstock") + "($1,$2,$3::json);", ['ops1','ops2', req.body], function(data) {
         rs.resp(res, 200, data.rows);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
-    }, 1)
+    }, 2)
 }
 
 openstock.saveWareOpeningStock = function saveWareOpeningStock(req, res, done)
