fb0a2115c34b5f4df49ce3502f3fdedc61ed8241
vivekpandey12 <vivek.pandey5188@gmail.com>
2017-02-13 18:29:19 +0530

    make new service budget

--- appmodule/budget.js
+++ appmodule/budget.js
@@ -2,40 +2,35 @@ var db = require("../db/dbservice.js");
 var rs = require("./util/resp.js");
 var globals = require("../globals.js");
 
-var jv = module.exports = {};
+var budget = module.exports = {};
 
-jv.getjv = function getjv(req, res, done) {
+budget.getBudget = function getBudget(req, res, done) {
     var params = [];
     var paramstr = "";
     var countr  = 1;
     
     switch (req.body.flag) {
-        case "docrange":
-            params = ['jv','jv1', req.body];
-            paramstr = "($1,$2,$3::json);";
-            countr = 2;
-            break;
-        case "details":
-            params = ['jv','jv1', req.body];
+        case "all":
+            params = ['budget','budget1', req.body];
             paramstr = "($1,$2,$3::json);";
             countr = 2;
             break;
         default:
-            params = ['jv', req.body];
+            params = ['budget', req.body];
             paramstr = "($1,'a',$2::json);";
             countr = 1;
             break;
     }
 
-    db.callProcedure("select " + globals.schema("funget_jv") + paramstr, params, function(data) {
+    db.callProcedure("select " + globals.schema("funget_budget") + paramstr, params, function(data) {
         rs.resp(res, 200, data.rows);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
     }, countr)
 }
 
-jv.savejv = function savejv(req, res, done) {
-    db.callFunction("select " + globals.schema("funsave_jv") + "($1::json);", [req.body], function(data) {
+budget.saveBudget = function saveBudget(req, res, done) {
+    db.callFunction("select " + globals.schema("funsave_budget") + "($1::json);", [req.body], function(data) {
         rs.resp(res, 200, data.rows);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
